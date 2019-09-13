ecbd737ed08cb674092dde0bcc46064aa43c5d08
Andrii Krasovskyi <arnio.if@gmail.com>
2019-09-14 02:23:36 +0300

    add drush

---
+++ drush.sh
@@ -0,0 +1,8 @@
+composer global require drush/drush:9.*
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+composer global update
+drush -y site:install standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=Example --account-name=admin --account-pass=admin
+cd /var/www/html/thunder-8.x-2.44/
+cp sites/default/default.settings.php sites/default/settings.php
+chmod a+w sites/default/settings.php
+chmod a+w sites/default
