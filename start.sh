c66f123edaf5709e34ee9867bb58e35e9f37ccb4
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-13 14:41:32 +0300

    edit sh

--- start.sh
+++ start.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+if [ -f /var/www/html/thunder-8.x-2.44/sites/default/default.settings.php ]
+then
+cp /var/www/html/thunder-8.x-2.44/sites/default/default.settings.php /var/www/html/thunder-8.x-2.44/sites/default/settings.php
+cat >> /var/www/html/thunder-8.x-2.44/sites/default/settings.php << EOF 
+$databases['default']['default'] = array (
+'database' => '${MYSQL_DATABASE}',
+'username' => '${MYSQL_USER}',
+'password' => '${MYSQL_PASSWORD}',
+'prefix' => '',
+'host' => '${HOSTNAME}',
+'port' => '3306',
+'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+'driver' => 'mysql',
+);
+EOF
+fi
 php-fpm7
 nginx -g "daemon off;"
 
