771d8a7225306c14d2cdaf29a8013aed316aaae6
Andrii Krasovskyi <arnio.if@gmail.com>
2019-09-14 23:21:52 +0300

    thunder update

--- drush.sh
+++ drush.sh
@@ -1,9 +1,7 @@
-apk add --no-cache mysql-client
-composer global require drush/drush:9.*
-export PATH="$HOME/.composer/vendor/bin:$PATH"
-composer global update
-cd /var/www/html/thunder-8.x-2.44/
+cd /var/www/html/thunder/
+composer require drush/drush:9.*
+composer update
 cp sites/default/default.settings.php sites/default/settings.php
 chmod a+w sites/default/settings.php
 chmod a+w sites/default
-drush -y site:install standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=Example --account-name=admin --account-pass=admin
+vendor/bin/drush -y site:install standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=Example --account-name=admin --account-pass=admin
