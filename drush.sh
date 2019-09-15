39146ef796ce1b26bede61737204cb1d2a593de6
Andrii Krasovskyi <arnio.if@gmail.com>
2019-09-15 19:53:33 +0300

    php change

--- drush.sh
+++ drush.sh
@@ -1,7 +1,6 @@
 cp /var/www/html/thunder/sites/default/default.settings.php /var/www/html/thunder/sites/default/settings.php
 chmod 755 /var/www/html/thunder/sites
 chmod 755 /var/www/html/thunder/sites/default
-chmod 755 /var/www/html/thunder/sites/default/files
 chmod a+w /var/www/html/thunder/sites/default/settings.php
 cd /var/www/html/thunder/ && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
 chmod 444 /var/www/html/thunder/sites/default/settings.php
