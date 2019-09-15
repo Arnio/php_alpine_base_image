69decd7080b122260186c4525d7d66d08f648b02
Andrii Krasovskyi <arnio.if@gmail.com>
2019-09-15 19:44:05 +0300

    php change

--- drush.sh
+++ drush.sh
@@ -1,7 +1,7 @@
-cd /var/www/html/thunder/
-composer global require drush/drush:9.*
-composer update --dry-run
-cp sites/default/default.settings.php sites/default/settings.php
-chmod a+w sites/default/settings.php
-chmod a+w sites/default
-vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=Example --account-name=admin --account-pass=admin
+cp /var/www/html/thunder/sites/default/default.settings.php /var/www/html/thunder/sites/default/settings.php
+chmod 755 /var/www/html/thunder/sites
+chmod 755 /var/www/html/thunder/sites/default
+chmod 755 /var/www/html/thunder/sites/default/files
+chmod a+w /var/www/html/thunder/sites/default/settings.php
+cd /var/www/html/thunder/ && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
+chmod 444 /var/www/html/thunder/sites/default/settings.php
