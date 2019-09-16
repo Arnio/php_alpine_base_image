3029ca778d5ad1d806cb5e03370a50f592579eb4
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-16 16:38:26 +0300

    change script

--- scripts/initapp.sh
+++ scripts/initapp.sh
@@ -1,14 +1,13 @@
 apk add -y mqsql-client
-for i in $(ls -d /var/www/html/thunder/sites/*/ ); do
-	FileName=$(find $i -type f)
-	if [ ! -z $FileName ]; then
-      		echo -e "Folder" "$i" "has some file(s) failed to be processed" "\n"
-	else
-        	echo -e "Folder" "$i" "is empty" "\n"
-            mv /tmp/sites/* /var/www/html/thunder/sites
-            chmod a+w /var/www/html/thunder/sites/
-            cd /var/www/html/thunder/ && composer require drush/drush:master && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
-            chown -R nginx:nginx /var/www/html/thunder/sites/            
-	fi
-done
+filecount=$(ls -At1 /var/www/html/thunder/sites/default/* 2>&- | wc -l)
+if [ $filecount -gt 3 ]; then
+        echo -e "The folder has $filecount some file(s) failed to be processed" "\n"
+else
+        echo -e "The folder has $filecount some file(s)" "\n"
+        mv -if /tmp/settings/*.* /var/www/html/thunder/sites/default/
+        chmod a+w -R /var/www/html/thunder/sites/default
+        cd /var/www/html/thunder/ && composer require drush/drush:master && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
+        chown -R nginx:nginx /var/www/html/thunder/sites/            
+fi
+
 
