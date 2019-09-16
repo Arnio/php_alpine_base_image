67b59f1d6086919e4128c353bd6f93c7a92ba31a
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-16 14:29:14 +0300

    change script

--- scripts/initapp.sh
+++ scripts/initapp.sh
@@ -5,7 +5,7 @@ for i in $(ls -d /var/www/html/thunder/sites/ ); do
       		echo -e "Folder" "$i" "has some file(s) failed to be processed" "\n"
 	else
         	echo -e "Folder" "$i" "is empty" "\n"
-            mv /tmp/sites /var/www/html/thunder/sites
+            mv /tmp/sites /var/www/html/thunder/
             chmod a+w /var/www/html/thunder/sites/
             cd /var/www/html/thunder/ && composer require drush/drush:master && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
             chown -R nginx:nginx /var/www/html/thunder/sites/            
