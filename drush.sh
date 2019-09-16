d079ee8f34fe18fdf2a1a1c3f1a55ca813165a7f
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-16 10:17:28 +0300

    edit sh

--- drush.sh
+++ drush.sh
@@ -1,6 +1,4 @@
 cp /var/www/html/thunder/sites/default/default.settings.php /var/www/html/thunder/sites/default/settings.php
-chmod 755 /var/www/html/thunder/sites
-chmod 755 /var/www/html/thunder/sites/default
 chmod a+w /var/www/html/thunder/sites/default/settings.php
 cd /var/www/html/thunder/ && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
-chmod 444 /var/www/html/thunder/sites/default/settings.php
+chown -R nginx:nginx /var/www/html/thunder/sites/default
