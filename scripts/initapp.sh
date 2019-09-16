34e8fdfc86dd1361a7509c9ecf3562332ce51153
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-16 17:22:45 +0300

    change script

--- scripts/initapp.sh
+++ scripts/initapp.sh
@@ -4,7 +4,7 @@ if [ $filecount -gt 3 ]; then
         echo -e "The folder has $filecount some file(s) failed to be processed" "\n"
 else
         echo -e "The folder has $filecount some file(s)" "\n"
-        mv -if /tmp/settings/*.* /var/www/html/thunder/sites/default/
+        mv /tmp/settings/*.* /var/www/html/thunder/sites/default/
         chmod a+w -R /var/www/html/thunder/sites/default
         cd /var/www/html/thunder/ && composer require drush/drush:master && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
         chown -R nginx:nginx /var/www/html/thunder/sites/            
