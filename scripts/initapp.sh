3cf99fa58ac01101a62ae6325de7e9a5de4076fd
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-16 15:51:15 +0300

    change script

--- scripts/initapp.sh
+++ scripts/initapp.sh
@@ -1,11 +1,11 @@
 apk add -y mqsql-client
-for i in $(ls -d /var/www/html/thunder/sites/ ); do
+for i in $(ls -d /var/www/html/thunder/sites/*/ ); do
 	FileName=$(find $i -type f)
 	if [ ! -z $FileName ]; then
       		echo -e "Folder" "$i" "has some file(s) failed to be processed" "\n"
 	else
         	echo -e "Folder" "$i" "is empty" "\n"
-            mv /tmp/sites /var/www/html/thunder/
+            mv /tmp/sites/* /var/www/html/thunder/sites
             chmod a+w /var/www/html/thunder/sites/
             cd /var/www/html/thunder/ && composer require drush/drush:master && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
             chown -R nginx:nginx /var/www/html/thunder/sites/            
