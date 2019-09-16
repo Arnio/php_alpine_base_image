fb961a6c956ae431aea636a24567e0c06bccbc62
Andrii Krasovskyi <arnio.if@gmail.com>
2019-09-16 20:29:24 +0300

    fix sh

--- scripts/initapp.sh
+++ scripts/initapp.sh
@@ -6,7 +6,7 @@ else
         echo -e "The folder has $filecount some file(s)" "\n"
         mv /tmp/settings/*.* /var/www/html/thunder/sites/default/
         chmod a+w -R /var/www/html/thunder/sites/default
-        cd /var/www/html/thunder/ && composer require drush/drush:master && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
+        cd /var/www/html/thunder/ && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
         chown -R nginx:nginx /var/www/html/thunder/sites/            
 fi
 
