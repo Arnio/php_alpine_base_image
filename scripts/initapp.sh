c32703f7cc68894ca97be3c25139bc0fe01cc003
Andrii Krasovskyi <akraso@softserveinc.com>
2019-09-18 14:10:25 +0300

    add base verify

--- scripts/initapp.sh
+++ scripts/initapp.sh
@@ -1,21 +1,31 @@
 apk add mysql-client
 RESULT=`MYSQL_PWD="$MYSQL_PASSWORD" mysql -h $MYSQL_HOST -u $MYSQL_USER -D $MYSQL_DATABASE -e 'SHOW TABLES' | grep -o node | wc -l`
-if [ $RESULT -lt 1 ]; then
-    echo YES
-        filecount=$(ls -At1 /var/www/html/thunder/sites/default/* 2>&- | wc -l)
-        if [ $filecount -gt 3 ]; then
-                echo -e "The folder has $filecount some file(s) failed to be processed" "\n"
-        else
-                echo -e "The folder has $filecount some file(s)" "\n"
-                mv /tmp/settings/*.* /var/www/html/thunder/sites/default/
-                chmod a+w -R /var/www/html/thunder/sites/default
-                cd /var/www/html/thunder/ && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
-                chown -R nginx:nginx /var/www/html/thunder/sites/            
-
-
-        fi
-else
-/var/www/html/thunder/vendor/bin/drush sql:connect --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE}
+if [ -f /var/www/html/thunder/sites/default/settings.php ]; then
+            mv /tmp/settings/*.* /var/www/html/thunder/sites/default/
 fi
+if [ $RESULT -lt 1 ]; then
+        chmod a+w -R /var/www/html/thunder/sites/default
+        cd /var/www/html/thunder/ && vendor/bin/drush -y si standard --db-url=mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}/${MYSQL_DATABASE} --site-name=SiteName --account-name=admin --account-pass=admin
+        chown -R nginx:nginx /var/www/html/thunder/sites/            
+      
+else
+if [ $(cat /var/www/html/thunder/sites/default/settings.php | grep "'database' => '${MYSQL_DATABASE}',") == ""]; then
+echo $(cat /var/www/html/thunder/sites/default/settings.php | grep "'database' => '${MYSQL_DATABASE}',")
+cat <<EOF | tee -a /var/www/html/thunder/sites/default/settings.php
+\$databases['default']['default'] = array (
+  'database' => '${MYSQL_DATABASE}',
+  'username' => '${MYSQL_USER}',
+  'password' => '${MYSQL_PASSWORD}',
+  'prefix' => '',
+  'host' => '${HOSTNAME}',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+EOF
+fi
+sed -i -e "s/update_free_access'] = FALSE;/update_free_access'] = TRUE;/g" /var/www/html/thunder/sites/default/settings.php
 
+fi
+chown -R nginx:nginx /var/www/html/thunder/sites/
 /var/www/html/thunder/vendor/bin/drush cr
